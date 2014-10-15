d527196a189e18ab672eabdf94932d9ec55ede40
cedricolivier <olivier.cedric@gmail.com>
2014-10-15 18:47:16 +0000

    modification bd mooc

--- mooc.sql
+++ mooc.sql
@@ -4,27 +4,9 @@ CREATE DATABASE IF NOT EXISTS mooc;
 
 USE mooc;
 
-CREATE TABLE IF NOT EXISTS users (
-       id INT AUTO_INCREMENT PRIMARY KEY,
-       login VARCHAR(30) NOT NULL 
-       );
-
-CREATE TABLE IF NOT EXISTS fiche (
-       id INT AUTO_INCREMENT PRIMARY KEY,
-       ftype VARCHAR(30) NOT NULL,
-       max_tmp TIMESTAMP
-       );
-
-CREATE TABLE IF NOT EXISTS cours (
-       id INT AUTO_INCREMENT PRIMARY KEY,
-       login VARCHAR(30) NOT NULL 
-       );
-
-CREATE TABLE IF NOT EXISTS fait (
-       idUsers INT,
-       idFiche INT,
-       temps TIMESTAMP NOT NULL,
-       moment DATE NOT NULL,
-       FOREIGN KEY (idUsers) REFERENCES users(id),
-       FOREIGN KEY (idFiche) REFERENCES cours(id)
-       );
+CREATE TABLE IF NOT EXISTS EventUser (
+       userId VARCHAR(30) NOT NULL,
+       eventName VARCHAR(30) NOT NULL,
+       dateEvent DATE NOT NULL,
+       idEvent VARCHAR(200) NOT NULL
+);
