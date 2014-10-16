5fd2e40738c96b38681b97ed73a0601cdd8d6fe3
cedricolivier <olivier.cedric@gmail.com>
2014-10-16 08:39:32 +0000

    id in table EventUser

--- mooc.sql
+++ mooc.sql
@@ -5,6 +5,7 @@ CREATE DATABASE IF NOT EXISTS mooc;
 USE mooc;
 
 CREATE TABLE IF NOT EXISTS EventUser (
+       id INT AUTO_INCREMENT KEY,
        userId VARCHAR(30) NOT NULL,
        eventName VARCHAR(30) NOT NULL,
        dateEvent DATETIME NOT NULL,
